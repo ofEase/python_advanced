f69758a7f8dfff09fd8ffb1363988db1548cd326
hualin.lai <hualin.lai1@gmail.com>
2026-07-18 22:39:46 +0800

    对表头列进行替换，换顺序，对整列进行条件操作

--- pandas_study/study01.py
+++ pandas_study/study01.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import pathlib
+import numpy as np
 
 ROOT_FILE_EMPLOYEE = r"D:\CODE\first_p\datas\员工数据表.xlsx"
 df_original = pd.read_excel(ROOT_FILE_EMPLOYEE)
 
+def costomize_title(column_name):
+    # 首字母变大写
+    return str(column_name).title()
 
 def review_colum():
     """
@@ -19,8 +23,19 @@ def review_colum():
     df_original.insert(sex_index - 1, '生日', value=birthday)
 
     series_name = df_original[df_original["姓名"].str.contains("刚")]
-    
-    print(series_name.sample(11))
 
+    # 对薪资低于5千的加0.6倍
+    salary = df_original.columns.get_loc("薪资")
+    df_original["薪资"]
+    df_original.insert(salary + 1, '加薪', 
+                       value=np.where(df_original["薪资"] < 10000, df_original["薪资"] * 1.5, df_original["薪资"]))
+    
+    df_original.rename(columns=costomize_title, inplace=True)
+
+    
+    print(df_original.head(10))
+    # print(series_name.sample(11))
+
+    df_original.rename()
 if __name__ == "__main__":
     review_colum()
