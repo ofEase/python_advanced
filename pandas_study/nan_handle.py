2cf5fe25a3d19e5e47e9357d9d17e3d1883e8631
hualin.lai <hualin.lai1@gmail.com>
2026-08-05 23:30:26 +0800

    学习pandas 空值的处理 1

--- pandas_study/nan_handle.py
+++ pandas_study/nan_handle.py
@@ -4,15 +4,30 @@ from pathlib import Path
 
 root_file = Path(r"D:\CODE\datas\员工数据表.xlsx")
 
+
 def nan_handle_01():
     df = pd.read_excel(root_file)
-    df.loc[df["部门"] == "技术部","薪资"] = np.nan
+    df.loc[df["部门"] == "技术部", "薪资"] = np.nan
 
-    df.loc[df["部门"]=="运营部", "性别"] = np.nan
+    df.loc[df["部门"] == "运维部", "性别"] = np.nan
+    # 统计查看空置
     # print(df.info())
-    print(df.isna().sum())
-    print(df.no)
+    # 是nan
+    # print(df.isna().sum())
+    # 不是nan
+    # print(df.notna())
 
+    # 删除空值行(默认),
+    # df = df.dropna()
+    # 所有字段为空值才会被删除
+    # df.dropna(how="all")
+    # 当指定的字段为空值的时候删除
+    # df.dropna(subset="性别")
+
+    # 填充缺失值
+    # 给空值的列填充固有值
+    df = df.fillna(value={"性别": "女", "薪资": 1})
+    print(df[df["部门"] == "运维部"].head(10))
 
 
 if __name__ == "__main__":
